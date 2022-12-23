442a6959422c765226bd5b83ad0fe43f47129086
Dmitrii Dolbik <dda1508@gmail.com>
2022-12-23 13:03:56 +0300

    add solution

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
+    public static void main(String[] args) {
+        FloatTypeCasting ftc = new FloatTypeCasting();
+        ftc.roundNumber(10.4f);
+    }
     public void roundNumber(float numberToBeRounded) {
+        int result = Math.round(numberToBeRounded);
+        System.out.println(result);
     }
 }
