cb5418f1d28ad97d43a56ef811f63e72d733e8f2
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-08 15:09:13 +0300

    updated3 solution

--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -8,7 +8,7 @@ public class OperatorPrecedence {
         System.out.println(eight);//eight is expected to be 8
         boolean firstBool = false;
         boolean secondBool = false;
-        boolean thirdBool = false;
+        boolean thirdBool = 1 == 1;
         boolean fourthBool = 4 <= 101;
         int varSix = 17;
 
